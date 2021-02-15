969a72ee7ee5035291a767fa9fc213f445a1e5a6
Genius Infomedia <73596856+gi-yt@users.noreply.github.com>
2021-02-15 11:51:38 +0000

    Update archfi.sh

--- archfi.sh
+++ archfi.sh
@@ -13,32 +13,17 @@ read -p "What Is Your ROOT PASSWORD. Note: Passwords entered during the install 
 read -p "what is The Username Of The User : " USERSNAME
 read -p "What is the password of the normal user : " USERSPASSWDLOL
 read -p "What Linux Kernel Do You Want to use (options are linux linux-zen linux-lts)" KERNEL
-read -p "What bootloader do you want to use (options are grub and systemd-boot BTW you cant use systemd-boot with legacy bios)" BOOTLOADER
 echo "DE, WM related stuff will be asked after the install finishes along with aur helper related stuff"
 echo "Thank You For Giving The Required Information $PNAME." 
 FILE="/sys/firmware/efi/efivars"
 if [ -e "$FILE" ]
 then
   echo "You are Using EFI"
-  umount -a
-  cat <<EOF | fdisk $IDISK
-  g
-  n
-
-
-  +512M
-  t
-  1
-  n
-
-
-  +30G
-  n
-
-
-
-  q
-  EOF
+  echo "Make Sure You Dont Have $IDISK mounted anywhere"
+  parted  $IDISK --script mklabel gpt
+            parted $IDISK --script ESP fat32 1MiB 513MiB
+            parted $disk_chk --script mkpart primary ext4 513MiB 29123MiB
+            parted $disk_chk --script mkpart primary 29123MiB -1
   partprobe
   part_1=("${$IDISK}1")
   part_2=("${IDISK}2")
@@ -55,47 +40,29 @@ then
   mount part_3 /mnt/home
   pacstrap /mnt base base-devel $KERNEL linux-firmware $headers
   genfstab -U /mnt >> /mnt/etc/fstab
-  cat <<CHROOT | arch-chroot /mnt
-  ln -sf /usr/share/zoneinfo/$TIMEZONEUSER /etc/localtime
-  timedatectl set-timezone $TIMEZONEUSER
-  echo "en_UTF-8 UTF-8" > /etc/locale.gen
-  locale-gen
-  touch /etc/locale.conf
-  echo "LANG=en_US.UTF-8" > /etc/locale.conf
-  export LANG=en_US.UTF-8
-  echo $HOSTNAMEOFNEWINSTALL > /etc/hostname
-   echo '127.0.0.1  localhost' | tee -a /etc/hosts
-   echo '::1        localhost' | tee -a /etc/hosts
-   echo '127.0.1.1  $HOSTNAMEOFNEWINSTALL' | tee -a /etc/hosts
-  cat <<PASS | passwd 
-  $ROOTPASSWD
-  $ROOTPASSWD
-  PASS
-  pacman -S grub efibootmgr os-prober networkmanager
-  grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
-  grub-mkconfig -o /boot/grub/grub.cfg
-  useradd -m -g users -G wheel $USERNAME
-  cat <<PASSUSER | passwd $USERNAME
-  $USERPASSWDLOL
-  $USERPASSWDLOL
-  PASSUSER
-  systemctl enable NetworkManager.service
-  CHROOT
+  arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONEUSER /etc/localtime
+  arch-chroot /mnt timedatectl set-timezone $TIMEZONEUSER
+  arch-chroot /mnt echo "en_UTF-8 UTF-8" > /etc/locale.gen
+  arch-chroot /mnt locale-gen
+  arch-chroot /mnt touch /etc/locale.conf
+  arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  arch-chroot /mnt export LANG=en_US.UTF-8
+  arch-chroot /mnt echo $HOSTNAMEOFNEWINSTALL > /etc/hostname
+  arch-chroot /mnt echo '127.0.0.1  localhost' | tee -a /etc/hosts
+  arch-chroot /mnt echo '::1        localhost' | tee -a /etc/hosts
+  arch-chroot /mnt echo '127.0.1.1  $HOSTNAMEOFNEWINSTALL' | tee -a /etc/hosts
+  arch-chroot /mnt echo -e "$ROOTPASSWD\n$ROOTPASSWD" | passwd root
+  arch-chroot /mnt pacman -S grub efibootmgr os-prober networkmanager
+  arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
+  arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+  arch-chroot /mnt useradd -m -g users -G wheel $USERNAME
+  arch-chroot /mnt echo -e "$USERPASSWDLOL\n$USERPASSWDLOL" | passwd $USERNAME
+  arch-chroot /mnt systemctl enable NetworkManager.service
   else
   echo "You are Using Legacy BIOS"
-  umount -a
-  cat <<BIOS | fdisk $IDISK
-  o
-  n
-
-
-  +30G
-  n
-
-
-
-  w
-  EFI
+  parted $IDISK --script mklabel msdos
+  parted $disk_chk --script mkpart primary ext4 1MiB 29123MiB
+  parted $disk_chk --script mkpart primary 29123MiB -1
   partprobe
   part_1=("${$IDISK}1")
   part_2=("${IDISK}2")
@@ -108,31 +75,23 @@ then
   mount part_2 /mnt/home
   pacstrap /mnt base base-devel $KERNEL linux-firmware $headers
   genfstab -U /mnt >> /mnt/etc/fstab
-  cat <<CHROOT | arch-chroot /mnt
-  ln -sf /usr/share/zoneinfo/$TIMEZONEUSER /etc/localtime
-  timedatectl set-timezone $TIMEZONEUSER
-  echo "en_UTF-8 UTF-8" > /etc/locale.gen
-  locale-gen
-  touch /etc/locale.conf
-  echo "LANG=en_US.UTF-8" > /etc/locale.conf
-  export LANG=en_US.UTF-8
-  echo $HOSTNAMEOFNEWINSTALL > /etc/hostname
-   echo '127.0.0.1  localhost' | tee -a /etc/hosts
-   echo '::1        localhost' | tee -a /etc/hosts
-   echo '127.0.1.1  $HOSTNAMEOFNEWINSTALL' | tee -a /etc/hosts
-  cat <<PASS | passwd 
-  $ROOTPASSWD
-  $ROOTPASSWD
-  PASS
-  pacman -S grub efibootmgr os-prober networkmanager
-  grub-install $IDISK
-  grub-mkconfig -o /boot/grub/grub.cfg
-  useradd -m -g users -G wheel $USERNAME
-  cat <<PASSUSER | passwd $USERNAME
-  $USERPASSWDLOL
-  $USERPASSWDLOL
-  PASSUSER
+  arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONEUSER /etc/localtime
+  arch-chroot /mnt timedatectl set-timezone $TIMEZONEUSER
+  arch-chroot /mnt echo "en_UTF-8 UTF-8" > /etc/locale.gen
+  arch-chroot /mnt locale-gen
+  arch-chroot /mnt touch /etc/locale.conf
+  arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  arch-chroot /mnt export LANG=en_US.UTF-8
+  arch-chroot /mnt echo $HOSTNAMEOFNEWINSTALL > /etc/hostname
+  arch-chroot /mnt echo '127.0.0.1  localhost' | tee -a /etc/hosts
+  arch-chroot /mnt echo '::1        localhost' | tee -a /etc/hosts
+  arch-chroot /mnt echo '127.0.1.1  $HOSTNAMEOFNEWINSTALL' | tee -a /etc/hosts
+  arch-chroot /mnt echo -e "$ROOTPASSWD\n$ROOTPASSWD" | passwd root
+  arch-chroot /mnt pacman -S grub os-prober networkmanager
+  arch-chroot /mnt grub-install --target=i386-pc $IDISK
+  arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+  arch-chroot /mnt useradd -m -g users -G wheel $USERNAME
+  echo -e "$USERPASSWDLOL\n$USERPASSWDLOL" | passwd USERNAME
   systemctl enable NetworkManager.service
-  CHROOT
 fi
 echo "Install Finished :) "
